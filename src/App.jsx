24602c15dbef2e1a6bca8bfff9bcaf7712765752
gilmariosantos1@gmail.com <gilmariosantos1@gmail.com>
2025-10-19 00:12:55 -0300

    Commit aula exemplo para os alunos

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    
+      <div className="App">
+        
+      <p className="text-3xl font-bold underline">
+        Bem Vindos, Senac Agradeçe sua presença!
+      </p>
+
+      <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+    />
+    <legend>Katherine Johnson</legend>
+    </div>
+  )
+
+}
+
+
+
+export default App
